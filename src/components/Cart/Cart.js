30003680dfe694056e7562b94b7a8051f50165e8
jakubjarek <71976031+jakubjarek@users.noreply.github.com>
2021-12-26 13:40:04 +0100

    change styling

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,21 +1,46 @@
+import { useEffect } from 'react';
+import { AiOutlineCloseSquare } from 'react-icons/ai';
+
 import CartItem from '../CartItem/CartItem';
 import CartModal from '../CartModal/CartModal';
 
-function Cart({ content }) {
+function Cart({ content, closeCartHandler }) {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.body.style.overflow = 'visible';
+    };
+  }, []);
+
   return (
     <CartModal>
-      <div className=" w-full flex flex-col max-w-7xl min-h-[24rem] bg-white rounded-lg drop-shadow-2xl p-4 m-4">
-        <span className="text-3xl font-bold">Your cart</span>
-        <ul className="">
-          {content.map(({id, title, price, image }) => (
-            <CartItem key={id} title={title} price={price} image={image} />
-          ))} 
-        </ul>
-        <div className="flex mt-auto w-full text-2xl font-bold">
-          <span className="ml-auto">
-            Total:
-            <span className="ml-2">${content.reduce((acc, item) => acc + item.price, 0)}</span>
-          </span>
+      <div className="w-full h-full flex flex-col items-center">
+        <div className="overflow-y-auto w-full max-w-[1184px]  p-4 pb-0 bg-white shadow-lg">
+          <div className="fixed w-full max-w-[1184px] top-0 right-1/2 translate-x-1/2 p-4 flex items-center justify-between bg-white shadow-lg">
+            <span className="text-xl font-bold">Your Cart ({content.length} items)</span>
+            <button className="p-1" onClick={closeCartHandler} aria-label="Close the cart">
+              <AiOutlineCloseSquare size={24} />
+            </button>
+          </div>
+          <div>
+            <div className="mt-12">
+              <ul className="pt-4">
+                {content.map(({ id, title, price, image }) => (
+                  <CartItem key={id} title={title} price={price} image={image} />
+                ))}
+              </ul>
+            </div>
+
+            <div className="flex p-4 bg-white border-t-2 border-solid border-slate-100 ">
+              <span className="mx-auto text-lg font-bold">
+                Total:
+                <span className="ml-2">
+                  ${content.reduce((acc, item) => acc + item.price, 0).toFixed(2)}
+                </span>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </CartModal>
--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,13 +1,20 @@
 function CartItem({ title, price, image }) {
   return (
-    <li className="mb-4">
-      <div className="p-4 h-40 max-w-xl flex">
-        <div className="h-full lg:min-w-[8rem] mr-8">
-          <img className="w-full h-full object-cover" src={image} alt="" aria-hidden="true" />
-        </div>
-        <div className="flex flex-col items-start pt-2">
-          <p className="mb-4">{title}</p>
-          <p className="font-bold text-xl">${price}</p>
+    <li className="border-b border-solid border-slate-100 py-5  ">
+      <div>
+        <div className="flex items-top">
+          <div className="w-20 h-28 md:w-32  lg:w-40 mr-6 flex-shrink-0">
+            <img
+              className="w-full h-full object-contain"
+              src={image}
+              alt=""
+              aria-hidden="true"
+            />
+          </div>
+          <div className="overflow-hidden">
+            <p className="mb-2 text-ellipsis overflow-hidden whitespace-nowrap">{title}</p>
+            <p className="font-bold">${price}</p>
+          </div>
         </div>
       </div>
     </li>
