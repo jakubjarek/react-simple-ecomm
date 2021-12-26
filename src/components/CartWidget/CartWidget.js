756c1712449f364fc52b72adab2734ecb201645a
jakubjarek <71976031+jakubjarek@users.noreply.github.com>
2021-12-26 13:39:39 +0100

    add aria for cart widget

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -6,7 +6,12 @@ function CartWidget({ show, cartLength, clickHandler }) {
   return (
     <>
       {show && (
-        <button onClick={clickHandler} className="fixed right-0 bottom-0 mr-4 mb-4 hover:bg-green-500 transition-colors flex w-14 h-14 items-center justify-center rounded-full bg-black">
+        <button
+          onClick={clickHandler}
+          className="fixed right-0 bottom-0 mr-4 mb-4 hover:bg-green-500 transition-colors flex w-14 h-14 items-center justify-center rounded-full bg-black"
+          aria-label="Open the cart"
+          aria-expanded="false"
+        >
           <div className="flex p-2">
             <span className="text-center font-bold text-xl text-white">{cartLength}</span>
             <FaShoppingCart className="text-white" size={28} />
