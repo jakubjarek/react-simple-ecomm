914475f91d0f6b416021285206fb5cb0fcffaaa3
jakubjarek <71976031+jakubjarek@users.noreply.github.com>
2021-12-26 11:15:25 +0100

    move useEffect to Cart component

--- src/components/CartModal/CartModal.js
+++ src/components/CartModal/CartModal.js
@@ -1,14 +1,4 @@
-import { useEffect } from 'react';
-
 function CartModal({ children }) {
-  useEffect(() => {
-    document.body.style.overflow = 'hidden';
-
-    return () => {
-      document.body.style.overflow = 'visible';
-    };
-  });
-
   return (
     <div className="fixed top-0 w-screen h-screen flex items-start justify-center bg-black/50">
       {children}
