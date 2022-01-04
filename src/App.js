15e09562efa5056f8ac1a8514d2689bc429e65b2
jakubjarek <71976031+jakubjarek@users.noreply.github.com>
2022-01-04 22:59:17 +0100

    use portal for displaying the modal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import ReactDOM from 'react-dom';
 import ProductsContainer from './components/ProductsContainer/ProductsContainer';
 import Cart from './components/Cart/Cart';
 import CartWidget from './components/CartWidget/CartWidget';
@@ -13,9 +14,11 @@ function App() {
     <>
       <ProductsContainer setCart={setCart} />
 
-      {isCartOpen && (
-        <Cart content={cart} onCloseCart={switchCartVisibility} setCart={setCart} />
-      )}
+      {isCartOpen &&
+        ReactDOM.createPortal(
+          <Cart content={cart} onCloseCart={switchCartVisibility} setCart={setCart} />,
+          document.getElementById('modal-wrapper')
+        )}
 
       {cart.length > 0 && !isCartOpen && (
         <CartWidget cartLength={cart.length} clickHandler={() => switchCartVisibility()} />
