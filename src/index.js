d6f379006330e02fa6dba4c28e47f0f8fbad6146
jakubjarek <71976031+jakubjarek@users.noreply.github.com>
2022-01-14 19:28:17 +0100

    split context files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { CartContextProvider } from './context/cart-context';
+import CartContextProvider from './context/CartContextProvider';
 
 ReactDOM.render(
   <React.StrictMode>
